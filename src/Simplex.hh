7b0bc7bf5c9040f7b6aaaaaf515fe6d836875dea
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2019-09-25 01:14:17 -0300

    Moving the result construction to Tabloid

--- src/Simplex.hh
+++ src/Simplex.hh
@@ -2,20 +2,6 @@
 #define SIMPLEX_HH
 #include "Tabloid.hh"
 
-enum class ResultType {
-    UNFEASIBLE,
-    ILIMITED,
-    LIMITED
-};
-
-class Result {
-    public:
-    ResultType type;
-    Vector certificate;
-    Fraction value;
-    Vector solution;
-};
-
 Result runSimplex(Tabloid &tabloid);
 
 #endif
